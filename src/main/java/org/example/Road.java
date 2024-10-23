44585b929f5d8f1e377a72d613327cbc9f0fb806
kofoworola <kay16th@yahoo.com>
2024-10-23 16:24:38 +0100

    feat:overrides checkTraffic

--- src/main/java/org/example/Road.java
+++ src/main/java/org/example/Road.java
@@ -3,6 +3,8 @@ package main.java.org.example;
 public class Road extends Environment<Vehicle>{
     @Override
     public void checkTraffic() {
-
+        for (Vehicle vehicle:traffic){
+            vehicle.move();
+        }
     }
 }
--- src/main/java/org/example/Water.java
+++ src/main/java/org/example/Water.java
@@ -3,6 +3,8 @@ package main.java.org.example;
 public class Water extends Environment<Swimmable>{
     @Override
     public void checkTraffic() {
-
+        for (Swimmable swimmable : traffic){
+            swimmable.swim();
+        }
     }
 }
