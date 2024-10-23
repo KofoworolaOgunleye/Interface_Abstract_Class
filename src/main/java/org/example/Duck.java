24ed1d4a3dacf14f9221c23784955ff6a78fb617
kofoworola <kay16th@yahoo.com>
2024-10-23 15:22:05 +0100

    feat: adds Pigeon and Duck classes

---
+++ src/main/java/org/example/Duck.java
@@ -0,0 +1,12 @@
+package main.java.org.example;
+
+public class Duck extends Bird implements Swimmable{
+    @Override
+    public void makeSound() {
+
+    }
+
+    @Override
+    public void fly() {
+    }
+}
---
+++ src/main/java/org/example/Pigeon.java
@@ -0,0 +1,12 @@
+package main.java.org.example;
+
+public class Pigeon extends Bird{
+    @Override
+    public void makeSound() {
+
+    }
+
+    @Override
+    public void fly() {
+    }
+}
