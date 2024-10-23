c975a734e18d861011da02035c62f930dfbbb3ed
kofoworola <kay16th@yahoo.com>
2024-10-23 16:16:53 +0100

    feat: overides checktraffic()

--- src/main/java/org/example/Sky.java
+++ src/main/java/org/example/Sky.java
@@ -3,6 +3,11 @@ package main.java.org.example;
 public class Sky extends Environment<Flyable>{
     @Override
     public void checkTraffic() {
+//        super.traffic.forEach(e -> e.fly());
+//        super.traffic.forEach(Flyable::fly);
 
+        for (Flyable flyable : traffic){
+            flyable.fly();
+        }
     }
 }
